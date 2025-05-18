c936b20adee3c8dc3f1942c191f54b371bd99cd9
tlxp <m4shm3llo@gmail.com>
2025-05-18 20:21:37 +0100

    Update index.js

--- Sprint1/index.js
+++ Sprint1/index.js
@@ -13,39 +13,39 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   loginForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const formData = new FormData(loginForm);
-    const data = {
-      username: formData.get('username'),
-      password: formData.get('password'),
-    };
+  e.preventDefault();
+  const formData = new FormData(loginForm);
+  const data = {
+    username: formData.get('username'),
+    password: formData.get('password'),
+  };
 
-    try {
-      const response = await fetch('http://localhost:3000/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-      });
-      const result = await response.json();
-      if (response.ok) {
-        localStorage.setItem('token', result.token);
-        localStorage.setItem('userRole', result.role);
-        const userRole = result.role;
-        if (userRole === 'Técnico') {
-          window.location.href = 'technician.html';
-        } else if (userRole === 'Cliente') {
-          window.location.href = 'dashboard.html';
-        } else {
-          alert('Role não suportado.');
-        }
+  try {
+    const response = await fetch('http://localhost:3000/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    if (response.ok) {
+      localStorage.setItem('token', result.token);
+      localStorage.setItem('userRole', result.role);
+      const userRole = result.role;
+      if (userRole === 'Técnico') {
+        window.location.href = 'technician.html';
+      } else if (userRole === 'Cliente') {
+        window.location.href = 'dashboard.html';
       } else {
-        alert(result.message);
+        alert('Role não suportado.');
       }
-    } catch (err) {
-      console.error('Erro ao fazer login:', err);
-      alert('Erro ao fazer login.');
+    } else {
+      alert(result.message);
     }
-  });
+  } catch (err) {
+    console.error('Erro ao fazer login:', err);
+    alert('Erro ao fazer login.');
+  }
+});
 
   registerForm.addEventListener('submit', async (e) => {
     e.preventDefault();
